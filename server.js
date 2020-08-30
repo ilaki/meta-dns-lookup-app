fb47bc539234c47350e5b338d5fe3842fd7d19d6
Ellakkian <ellakkian94@gmail.com>
2020-08-31 01:52:27 +0530

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
