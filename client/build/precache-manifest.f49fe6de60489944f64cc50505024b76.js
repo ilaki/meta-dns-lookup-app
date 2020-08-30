a32cadacc61e42aa2c0ea4c6fbbb125a1a85bae7
Ellakkian <ellakkian94@gmail.com>
2020-08-31 02:34:31 +0530

    mfix

---
+++ client/build/precache-manifest.f49fe6de60489944f64cc50505024b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921472d354658f7072fdb335310c6823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c1b84d6db393d353e8a",
+    "url": "/static/css/main.4c938188.chunk.css"
+  },
+  {
+    "revision": "07f110efa603ce51a24a",
+    "url": "/static/js/2.4d5e2376.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d5e2376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1b84d6db393d353e8a",
+    "url": "/static/js/main.1f6b5bb4.chunk.js"
+  },
+  {
+    "revision": "d4278dfeef37d6213491",
+    "url": "/static/js/runtime-main.c8b35ce6.js"
+  }
+]);
